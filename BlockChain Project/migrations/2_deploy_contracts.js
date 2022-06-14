86a3022ce11264937eb6e81ab06647998043a5b3
Giselle <47361500+gisgiselle@users.noreply.github.com>
2022-06-14 15:30:54 -0500

    Comments

--- BlockChain Project/migrations/2_deploy_contracts.js
+++ BlockChain Project/migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+//we require the contract we've created
+
 const TodoList = artifacts.require("./TodoList.sol");
 
 module.exports = function(deployer) {
+  //We retrieved a deployed instance of the contract with the deployed() function,
   deployer.deploy(TodoList);
 };
